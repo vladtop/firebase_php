858237e108605444a9305cf79f537252fcac79f0
Vladislav Dimitrov <dimitrov.vlad@gmail.com>
2018-12-27 21:28:58 +0200

    google service account connection

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/google-service-account.json');
+
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+    // The following line is optional if the project id in your credentials file
+    // is identical to the subdomain of your Firebase project. If you need it,
+    // make sure to replace the URL with the URL of your project.
+    //->withDatabaseUri('https://my-project.firebaseio.com')
+    ->create();
+
+$database = $firebase->getDatabase();
+
+$newPost = $database
+    ->getReference('blog/posts')
+    ->push([
+        'title' => 'Post title',
+        'body' => 'This should probably be longer.'
+    ]);
+
+echo '<pre>';
+
+print_r($newPost->getKey());
+print_r($newPost->getUri());
+
+$newPost->getChild('title')->set('Changed post title');
+print_r($newPost->getValue());
+$newPost->remove();
